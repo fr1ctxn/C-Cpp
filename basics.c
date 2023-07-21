c7ad9ff7ccae64cda835d4275e81ee4e80c53350
fr1ctxn <baazmannshinchan@outlook.com>
2023-07-21 21:46:39 +0530

    multiplication_table

--- basics.c
+++ basics.c
@@ -3,7 +3,7 @@
 int main(){
 
 //Taking user input
-    
+   /* 
     char name[100]; //declaration of taking input in characters, where the limit of characters is 100
 
     printf("What is your name?: ");
@@ -12,6 +12,49 @@ int main(){
     printf("hello, %s!\n", name);
     return 0;
 
+*/
+/*
 
+    TO TAKE INPUT AND CHECK IF IT IS NEGATIVE/POSITIBE
+
+    int number;
+
+    printf("Write any number: ");
+    scanf("%d", &number);
+
+    if (number > 0)
+    {
+      printf("The given number is positive");
+    }
+    else if(number == 0)
+    {
+     printf("The number is zero,i.e, neither negative nor positive");
+    }
+    else
+    {
+      printf("The number is negative");
+    }
+    
+    return 0;
+
+
+*/
+
+/*
+
+    int value, i;
+
+    printf("enter any number: ");
+    scanf("%d", &value);
+
+    printf("Multiplication table for %d: \n", value);
+
+    for(i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", value, i, value * i);
+    }
+
+    return 0;
+*/
 
 }
