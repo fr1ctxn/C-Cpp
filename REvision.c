fad5f5eacbf20e3bf33ce397cabcad466eed085e
fr1ctxn <baazmannshinchan@outlook.com>
2023-09-07 07:42:51 +0530

    rename

---
+++ REvision.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+    int main(){
+        //Question no 1
+        printf("Hello, world\n");
+
+        //Question no 2 
+        int num = 42;
+        printf("%d\n", num);
+
+        int num1, num2;
+        
+        
+
+    }
